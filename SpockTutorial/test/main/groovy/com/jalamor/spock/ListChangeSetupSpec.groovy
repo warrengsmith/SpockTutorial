b1f3d0f96bb0585394acf4f379745b8a3fa2350d
Warren Smith <warrengsmith@yahoo.com>
2015-10-28 22:21:52 -0400

    Create ListChangeSetupSpec to demonstrate setup, given and implicit initialization blocks

---
+++ SpockTutorial/test/main/groovy/com/jalamor/spock/ListChangeSetupSpec.groovy
@@ -0,0 +1,47 @@
+package com.jalamor.spock
+
+     import spock.lang.Specification
+      
+     class ListChangeSetupSpec extends Specification {
+      
+         def 'Adding an item to a list changes its size using setup block'() {
+             setup: 'an initial list with 4 items' // explicit setup block
+             def list  = ['a', 'b', 'c', 'd']
+  
+             expect:
+             list.size() == 4
+  
+             when: 'an element is added to the list'
+             list << 'e' 
+      
+             then: 'expect list size to be 5'
+             list.size() == 5
+         }
+  
+         def 'Adding items to the list changes its size using given alias'() {
+             given: 'that we have 4 items in a list' // explicit given alias
+             def list  = ['a', 'b', 'c', 'd']
+  
+             expect:
+             list.size() == 4
+  
+             when: 'an element is added to the list'
+             list << 'e' 
+      
+             then: 'expect list size to be 5'
+             list.size() == 5
+         }
+  
+         def 'Adding items to the list changes its size using implicit setup'() {
+             def list  = ['a', 'b', 'c', 'd'] // no given or setup block, just implicit setup
+  
+             expect:
+             list.size() == 4
+  
+             when: 'an element is added to the list'
+             list << 'e' 
+      
+             then: 'expected list size to be 5'
+             list.size() == 5
+         }
+     }
