c3e2b85f38169f0193a3482e837346124d469a16
Warren Smith <warrengsmith@yahoo.com>
2015-10-28 22:49:28 -0400

    Add Javadoc to FizzBuzzGroovy and FizzBuzzGroovyIdiomatic

--- SpockTutorial/src/main/groovy/com/jalamor/spock/FizzBuzzGroovy.groovy
+++ SpockTutorial/src/main/groovy/com/jalamor/spock/FizzBuzzGroovy.groovy
@@ -1,16 +1,40 @@
 package com.jalamor.spock
 
+/**
+ * FizzBuzz program prints the numbers from 1 to 100, but for multiples of three
+ * it prints “Fizz” and for multiples of five it prints “Buzz”. For numbers
+ * which are multiples of both three and five it prints “FizzBuzz”.
+ * 
+ * @author Warren Smith
+ */
 public class FizzBuzzGroovy {
 
-   static void main (String... args) {
-	   (1..100).collect {  // Loop from 1 to 100 and for each number, collect the
-		   fizzBuzz(it)    // result of calling the fizzBuzz function using a closure
-	   }.each {println it} // then for each element in the collection, print it 
-   }
+	/**
+	 * Loop from 1 to 100 and call the fizzBuzz function for each number,
+	 * printing the result to the console
+	 *
+	 * @param args
+	 */
+	static void main (String... args) {
+		(1..100).collect {  // Loop from 1 to 100 and for each number, collect the
+			fizzBuzz(it)    // result of calling the fizzBuzz function using a closure
+		}.each {println it} // then for each element in the collection, print it
+	}
 
-   static String fizzBuzz(num) {
-	   String result = num % 3 == 0 ? 'Fizz' : ''
-	   result += num % 5 == 0 ? 'Buzz' : ''
-	   result == '' ? num : result
-   }
+	/**
+	 * Method fizzBuzz. 
+	 * 1. initialize a place to append our results
+	 * 2. if the number divides by 3 exactly, append 'Fizz'
+	 * 3. if the number divides by 5 exactly, append 'Buzz'
+	 * 4. if it did not divide by 3 or 5, append the number
+	 * 5. return the result (either "Fizz", "Buzz", "FizzBuzz", or the number passed in)
+	 * 
+	 * @param number
+	 * @return String
+	 */
+	static String fizzBuzz(num) {
+		String result = num % 3 == 0 ? 'Fizz' : ''
+		result += num % 5 == 0 ? 'Buzz' : ''
+		result == '' ? num : result
+	}
 }
--- SpockTutorial/src/main/groovy/com/jalamor/spock/FizzBuzzGroovyIdiomatic.groovy
+++ SpockTutorial/src/main/groovy/com/jalamor/spock/FizzBuzzGroovyIdiomatic.groovy
@@ -1,7 +1,25 @@
 package com.jalamor.spock
 
+/**
+ * FizzBuzz program prints the numbers from 1 to 100, but for multiples of three
+ * it prints “Fizz” and for multiples of five it prints “Buzz”. For numbers
+ * which are multiples of both three and five it prints “FizzBuzz”.
+ *
+ * @author Warren Smith
+ */
 class FizzBuzzGroovyIdiomatic {
- 
+
+	/**
+	 * Loop from 1 to 100 and for each use a closures to print the result of evaluating the G-String, unless it's
+	 * null in which case return the number (i.e. implicit it). The G-String uses ternary operators 
+	 * to determine if it should append 'Fizz' or 'Buzz' or nothing to the result and String interpolation using 
+	 * the ${} notation to build the G-String (the result of this could finally be null (if nothing divides exactly 
+	 * by 3 or 5), 'Fizz' (if it divides by 3), 'Buzz' (if it divides by 5), or 'FizzBuzz' (if it divides by both). 
+	 * It uses the Elvis operator to figure out if the result of the G-String evaluation was null and if it was it
+	 * prints the number (as mentioned earlier).
+	 * 
+	 * @param args
+	 */
     static void main (String... args) {
 		(1..100).each{println "${it%3?'':'Fizz'}${it%5?'':'Buzz'}" ?: it }
     }
