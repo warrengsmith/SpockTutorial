99593dbee51f4bdf950d6079997cef6ee7ca2fb6
Warren Smith <warrengsmith@yahoo.com>
2015-11-16 23:38:06 -0500

    Tidy up

--- SpockTutorial/test/main/groovy/com/jalamor/spock/FizzBuzzRandomSpec.groovy
+++ SpockTutorial/test/main/groovy/com/jalamor/spock/FizzBuzzRandomSpec.groovy
@@ -31,20 +31,20 @@ class FizzBuzzRandomSpec extends Specification {
 	 */
 	def 'FizzBuzz Mock Example'() {
 		
-			given:
-			Random random = Mock()
-			FizzBuzzRandom fbr = new FizzBuzzRandom()
-			
-			when:
-			def resultList = fbr.getRandomizedFizzBuzzList(random)
-			
-			then:
-			1 * random.nextInt(100) >> 5
-			resultList.size() == 5
-			resultList[0] == '1'
-			resultList[1] == '2'
-			resultList[2] == 'Fizz'
-			resultList[3] == '4'
-			resultList[4] == 'Buzz'
-		}
+		given:
+		Random random = Mock()
+		FizzBuzzRandom fbr = new FizzBuzzRandom()
+		
+		when:
+		def resultList = fbr.getRandomizedFizzBuzzList(random)
+		
+		then:
+		1 * random.nextInt(100) >> 5
+		resultList.size() == 5
+		resultList[0] == '1'
+		resultList[1] == '2'
+		resultList[2] == 'Fizz'
+		resultList[3] == '4'
+		resultList[4] == 'Buzz'
+	}
 }
